f5b72beff46ccef5a98c96c55f60c3df7c775d85
Brandon-Mount <bmount44@gmail.com>
2026-07-22 14:49:51 -0600

    fixed for render

--- swagger.js
+++ swagger.js
@@ -1,15 +1,26 @@
+require("dotenv").config();
+
 const swaggerAutogen = require("swagger-autogen")();
+
+const isProduction = Boolean(process.env.API_HOST);
 
 const doc = {
   info: {
     title: "Game Library API",
     description: "CRUD API for games and game developers",
   },
-  host: "localhost:3000",
-  schemes: ["http"],
+
+  host: process.env.API_HOST || "localhost:3000",
+
+  schemes: isProduction ? ["https"] : ["http"],
 };
 
 const outputFile = "./swagger-output.json";
-const endpointsFiles = ["./routes/index.js"];
+
+const endpointsFiles = [
+  "./routes/index.js",
+  "./routes/games.js",
+  "./routes/developers.js",
+];
 
 swaggerAutogen(outputFile, endpointsFiles, doc);
